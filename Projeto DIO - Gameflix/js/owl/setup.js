43f63d320576cdd904fb1bfbd2f9b3c62baa82e5
renawmontanari <regiangelica@gmail.com>
2022-04-23 21:05:50 -0300

    Desafio DIO Netflix

---
+++ Projeto DIO - Gameflix/js/owl/setup.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    items:1,
+    merge:true,
+    loop:true,
+    margin:10,
+    video:true,
+    lazyLoad:true,
+    center:true,
+    responsive:{
+        480:{
+            items:2
+        },
+        600:{
+            items:4
+        }
+    }
+})
